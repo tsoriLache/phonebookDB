ab982d12b84c4f65033fe3a4e09f8194a2bb16e6
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-09 14:42:31 +0200

    added toJSON

--- server/models/contact.js
+++ server/models/contact.js
@@ -5,5 +5,12 @@ const contactSchema = new mongoose.Schema({
     number: String,
   })
   
+contactSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
 
 module.exports = mongoose.model('Contact', contactSchema);
