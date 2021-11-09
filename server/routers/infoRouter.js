a6dd1764a14c8beb35fcbfbe0be8ae466bf4f5e7
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-09 13:07:17 +0200

    added missing app&func

--- server/routers/infoRouter.js
+++ server/routers/infoRouter.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
+const {getLength} = require('../lib/dummyDB');
+
 
 app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${getLength()} people\n ${new Date().toString()}`)
