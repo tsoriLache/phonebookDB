bf32e24de2d200d9f9f28f8da00218b83836c419
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-11-09 17:13:00 +0200

    add prettier

--- server/routers/personsRouter.js
+++ server/routers/personsRouter.js
@@ -3,30 +3,30 @@ const router = express.Router();
 // const {isNumberValid} = require('../helpers/validation');
 const Contact = require('../models/contact');
 
-router.get('/', async(request, response) => {
-    response.json(await Contact.find({}))
-})
+router.get('/', async (request, response) => {
+  response.json(await Contact.find({}));
+});
 
-router.get('/:id', async(request, response) => {
-    const _id = (request.params.id);
-    try{
-        response.json((await Contact.find({_id}))[0])
-    }catch{
-        response.status(400);
-        response.send('id is not found');
-    }
-})
+router.get('/:id', async (request, response) => {
+  const _id = request.params.id;
+  try {
+    response.json((await Contact.find({ _id }))[0]);
+  } catch {
+    response.status(400);
+    response.send('id is not found');
+  }
+});
 
-router.delete('/:id', async(request, response) => {
-    const id = (request.params.id);
-    try{
-        await Contact.findOneAndRemove({id})
-        response.status(204).end()
-    }catch{
-        response.status(400);
-        response.send('id is not found');
-    }
-})
+router.delete('/:id', async (request, response) => {
+  const id = request.params.id;
+  try {
+    await Contact.findOneAndRemove({ id });
+    response.status(204).end();
+  } catch {
+    response.status(400);
+    response.send('id is not found');
+  }
+});
 
 router.post('/', async (request, response) => {
     const {name,number} = request.body;
@@ -43,7 +43,10 @@ router.post('/', async (request, response) => {
         response.status(400);
         response.send('The name or number is missing');
     }
-})
-
+  } else {
+    response.status(400);
+    response.send('The name or number is missing');
+  }
+});
 
 module.exports = router;
